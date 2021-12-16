a752eedd10852a3f1e68a02989df4c6f5a754e50
Kerkerek <96265230+Kerkerek@users.noreply.github.com>
2021-12-17 02:44:24 +0500

    Create barmite.py

---
+++ barmite.py
@@ -0,0 +1,23 @@
+import vk_api 
+from vk_api.longpoll import VkEventType, VkLongPoll 
+import time 
+  
+token = vk_api.VkApi(token="сырник гений") 
+lox = VkLongPoll(token) 
+
+def send(id, text, msgid, att): 
+ token.method('messages.send',{'chat_id' : id, 'message' : text, 'random_id' : 0, 'attachment' : att, 'reply_to':msgid })
+ 
+while True: 
+ for event in lox.listen(): 
+  if event.type == VkEventType.MESSAGE_NEW: 
+   if event.from_chat: 
+    idUser = event.user_id 
+    id = event.chat_id 
+    msg = event.text.lower() 
+    msgid = event.message_id
+    textspl = msg.split()
+    
+   if msg.startswith("бармить"):
+     send(id, f"Ты сбармил {'  '.join(textspl[1:])}",msgid, '')
+
